b25025fcd8d73a248e921049f84407f4c0467cfa
Nyde2283 <89605730+Nyde2283@users.noreply.github.com>
2023-04-20 12:33:00 +0200

    PR: Ajout du header (#1)
    * Add HTML elements
    
    * Style header
    
    * Change font to 'Inter'

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<Header></Header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+	return (
+		<header id="header">
+			<div id="profilpicture"></div>
+			<p id="name">
+				chevereau
+				<br />
+				edwyn
+			</p>
+		</header>
+	);
+}
+
+export default Header;
