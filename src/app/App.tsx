58d16711429e2af8842bb5af7eeecaa392ebf31e
Nyde2283 <edwynchev@gmail.com>
2023-04-21 15:59:08 +0200

    Add TextMedia

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import Header from './components/Header';
+import TexteMedia from './components/TexteMedia/TexteMedia';
+import { legendeContent } from '../assets/legendeContent';
+import { carrouselImgC1, carrouselImgC2, carrouselImgC3 } from './importator';
 import './App.css';
 
 function App() {
 	return (
 		<div className="app">
 			<Header />
+			<TexteMedia
+				carrouselImages={carrouselImgC1}
+				legendcontent={legendeContent[0]}
+				rightDirection={true}
+			/>
+			<TexteMedia
+				carrouselImages={carrouselImgC2}
+				legendcontent={legendeContent[1]}
+				rightDirection={false}
+			/>
+			<TexteMedia
+				carrouselImages={carrouselImgC3}
+				legendcontent={legendeContent[1]}
+				rightDirection={true}
+			/>
 		</div>
 	);
 }
--- src/app/components/TexteMedia/Legende/Legende.tsx
+++ src/app/components/TexteMedia/Legende/Legende.tsx
@@ -9,7 +9,7 @@ function Legende({ content }: LegendeProps) {
 	const title = content[0];
 	const description = content[1];
 	return (
-		<div className="Legende">
+		<div className="legende">
 			{title}
 			{description}
 		</div>
